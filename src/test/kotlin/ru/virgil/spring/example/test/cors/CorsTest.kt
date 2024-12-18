efec3ebe9abbeb66e579174df2c9034448e5c81d
ViRGiL175 <virgil7g@gmail.com>
2024-12-18 18:56:02 +0400

    Add CORS test, update module

---
+++ src/test/kotlin/ru/virgil/spring/example/test/cors/CorsTest.kt
@@ -0,0 +1,48 @@
+package ru.virgil.spring.example.test.cors
+
+import net.datafaker.Faker
+import net.pearx.kasechange.toKebabCase
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.http.HttpHeaders
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import ru.virgil.spring.example.roles.user.WithMockFirebaseUser
+import ru.virgil.spring.tools.security.cors.CorsProperties
+import ru.virgil.spring.tools.toolsBasePackage
+
+// todo: перенести в Spring-модуль
+@DirtiesContext
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockFirebaseUser
+@ComponentScan(toolsBasePackage)
+class CorsTest @Autowired constructor(
+    private val mockMvc: MockMvc,
+    private val corsProperties: CorsProperties,
+    private val faker: Faker,
+) {
+
+    @Test
+    fun testAllowedOrigin() {
+        val randomOrigin = corsProperties.origins.random()
+        mockMvc.get("/ping") { header(HttpHeaders.ORIGIN, randomOrigin) }
+            .andExpect {
+                status { isOk() }
+                header { string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, randomOrigin) }
+            }
+            .andDo { print() }
+    }
+
+    @Test
+    fun testNotAllowedOrigin() {
+        val wrongOrigin = faker.domain().validDomain(faker.company().name().toKebabCase())
+        mockMvc.get("/ping") { header(HttpHeaders.ORIGIN, wrongOrigin) }
+            .andExpect { status { isForbidden() } }
+            .andDo { print() }
+    }
+}
